3c88acfbbbf05010a5421ad80e381d9c588d8702
Felix <aguilarfnam@gmail.com>
2021-05-02 22:06:29 -0500

    Change the color of backgroud and font

--- src/App.js
+++ src/App.js
@@ -2,11 +2,26 @@ import './App.css';
 
 import QuoteBox from './QuoteBox';
 
+
+
 function App() {
+
+  const colors = ['72147e', 'f21170', 'fa9905', 'ff5200', '23049d', 'aa2ee6', 'ff79cd', 'ffc996', 'ff8474', '9f5f80', '583d72','fdca40', 'a799b7'];
+  const colorRandom = () => {
+    let num =Math.floor(Math.random() * (colors.length));
+    return colors[num];
+  }
+
+  let num = colorRandom();
+
+  const bgColor = {
+    backgroundColor: `#${num}`
+  }
+
   return (
-    <div className="App">
+    <div className="App" style = { bgColor }>
       <header className="App-header">
-        <QuoteBox />
+        <QuoteBox color = {num}/>
       </header>
     </div>
   );
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -2,8 +2,16 @@ import {useState} from 'react';
 import {quotes} from './quotes.json';
 import Quote from './Quote';
 
+import ReactDOM from 'react-dom';
+import App from './App';
+import React from 'react';
 
-const QuoteContainer = () => {
+
+const QuoteBox = ( {color} ) => {
+
+    const fontColor = {
+        color: `#${color}`
+    }
 
     const getRandom = () => {
         return Math.floor(Math.random() * (quotes.length));
@@ -12,15 +20,23 @@ const QuoteContainer = () => {
     const [ {quote,author}, setQuote] = useState( quotes[getRandom()] );
 
     return (
-        <div className= "quoteCont" >
+        <div className= "quoteCont" style = { fontColor }>
             <Quote quote= { quote } author = { author } />
             <div className = "btnBox" >
                 <button className = "btn" onClick = { () => alert('Me dio amsiedad') } >Twitt</button>
-                <button className = "btn" onClick = { () => setQuote(quotes[getRandom()]) } >New Quote</button>
+                <button className = "btn" onClick = { () => {
+                    setQuote(quotes[getRandom()])
+                    ReactDOM.render(
+                        <React.StrictMode>
+                            <App />
+                        </React.StrictMode>,
+                        document.getElementById('root')
+                    );
+                    } } >New Quote</button>
             </div>
         </div>
     )
 
 }
 
-export default QuoteContainer;
+export default QuoteBox;
